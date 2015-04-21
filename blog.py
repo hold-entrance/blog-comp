2b8b82f457c47a7bdae63ef3ef0c30ed072251ba
Holden Ranz <ranz.holden@gmail.com>
2015-04-21 13:11:22 -0400

    Preliminary database formatted and configured

--- blog.py
+++ blog.py
@@ -13,11 +13,13 @@ from flask import g
 import sqlite3
 import os
 
+from functools import wraps
+
 # configuration 
-DATABASE = 'blog.db'
+DATABASE = 'sports_stats.db'
 USERNAME = 'admin'
 PASSWORD = 'hard_to_guess'
-SECRET_KEY = str(os.random(24))
+SECRET_KEY = os.urandom(24)
 
 app = Flask(__name__)
 
@@ -27,6 +29,17 @@ app.config.from_object(__name__)
 # function use for connecting to the database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+# define login_required decorator
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash ('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 # define the root path, i.e. the login page
 @app.route('/', methods=['GET', 'POST'])
@@ -43,14 +56,40 @@ def login():
     
 # define the main page that will be displaying the blog posts
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('SELECT * FROM soccer')
+    posts = [dict(season=row[0], player=row[1], club=row[2], competition=row[3], goals=row[4]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
+
+# define function for adding and updating posts
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    season = request.form['season']
+    player = request.form['player']
+    club = request.form['club']
+    competition = request.form['competition']
+    goals = request.form['goals']
+    if not season or not player or not club or not competition or not goals:
+        flash("All fields are required for a new entry. Please try again.")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('INSERT INTO soccer (season, player, club, competition, goals) values (?, ?, ?, ?, ?)',\
+                     [season, player, club, competition, goals])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
 
 # define the logout path
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You have been logged out. See you later!')
-
+    return redirect(url_for('login'))
 if __name__ == '__main__':
     app.run(debug=True) 
